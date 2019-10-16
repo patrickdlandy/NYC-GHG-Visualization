1859a02a10f3c08dbc15b1ec2f8a21d60c6c6a30
Patrick Landy <patrickdlandy@gmail.com>
2019-10-16 17:36:34 -0400

    Push progress on sunburst example in index.html for now

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// import './styles/index.scss';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+})
+
+//everything is bundled into main.js by webpack and we just include a link to "main"
+
+
+    
